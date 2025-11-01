4cf9023b3706caf9c4e1255139edb974ac581636
Alex9003 <agl.luchko@gmail.com>
2025-11-01 14:09:15 +0200

    modified front

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template,request,jsonify
 import os
 
@@ -11,13 +10,46 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    cars = [
+        {
+            'img': '01.avif', 
+            'title': 'CUPRA Terramar MID 204к.с DSG 4Drive', 
+            'descriptin': 'CUPRA Terramar MID 204к.с DSG 4Drive опис'
+        },
+        {
+            'img': '02.jpeg', 
+            'title': 'CUPRA Terramar VZ 265к.с DSG 4Drive', 
+            'descriptin': 'CUPRA Terramar VZ 265к.с DSG 4Drive опис'
+        },
+        {'img': '03.jpeg', 'title': 'Formentor VZ 2.0 TSI 333кс DSG 4Drive', 'descriptin': 'Formentor VZ 2.0 TSI 333кс DSG 4   Drive опис'}
+    ]
+    
+    cars_for_sale = [
+        {
+            'img': 'CUPRATerramarMID204к.с.DSG4Drive.jpg',
+            'title': 'CUPRA Terramar MID 204к.с DSG 4Drive',
+            'descriptin': 'CUPRA Terramar MID 204к.с DSG 4Drive опис'
+        },
+        {
+            'img': 'CUPRATerramarVZ265к.с.DSG4Drive.jpg', 
+            'title': 'CUPRA Terramar VZ 265к.с DSG 4Drive',
+            'descriptin': 'CUPRA Terramar VZ 265к.с DSG 4Drive опис',            
+        },
+        {
+            'img': 'FormentorVZ2.0TSI333ксDSG4Drive.jpg',
+            'title': 'Formentor VZ 2.0 TSI 333кс DSG 4Drive',
+            'descriptin': 'Formentor VZ 2.0 TSI 333кс DSG 4 Drive опис' 
+        }
+    ]
+    return render_template('index.html', cars=cars, cars_for_sale=cars_for_sale)
 
 @app.route('/form', methods=['POST'])
 def handle_form():
     user_text = request.form.get('user-text')
     user_number = request.form.get('user-number')
     return f'Надісланоб текст: [{user_text}], число: [{user_number}]'
+
+
 
 @app.route('/save-data', methods=['POST'])
 def save_data():
