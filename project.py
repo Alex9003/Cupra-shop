a341abc37dcded5c3e3d3d98d3aa323f61ecbff1
Alex9003 <agl.luchko@gmail.com>
2025-11-08 14:15:19 +0200

    login

--- project.py
+++ project.py
@@ -1,15 +1,38 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, render_template,request,jsonify, redirect, url_for, session
 import os
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-
+USER = {
+    'useremail': 'text@gmail.com',
+    'password': '1'
+}
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_text'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/', methods=['GET'])
 def home():
+    user = ''
+    if 'keyuser' in session:
+        user = f"""
+        <h2>Вітаю {session['keyuser']}! Ви увійшли в систему.</h2>
+        """
+
     cars = [
         {
             'img': '01.avif', 
@@ -41,7 +64,7 @@ def home():
             'descriptin': 'Formentor VZ 2.0 TSI 333кс DSG 4 Drive опис' 
         }
     ]
-    return render_template('index.html', cars=cars, cars_for_sale=cars_for_sale)
+    return render_template('index.html', cars=cars, cars_for_sale=cars_for_sale, user=user)
 
 @app.route('/form', methods=['POST'])
 def handle_form():
@@ -69,6 +92,47 @@ def get_data():
 
     text = [line + 'br' for line in lines]
     return jsonify({'data': text}), 200
+
+@app.route('/register', methods=['GET'])
+def register():
+    if request.method == 'POST':
+        useremail = request.form.get('email')
+        password = request.form.get('password')
+
+
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        new_user = User(email=useremail, password=hashed_password) 
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    
+
+
+            
+
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        useremail = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=useremail).first()
+        if user and check_password_hash(user.password, password):            
+            
+            session['keyuser'] = useremail
+            return redirect(url_for('home'))
+        else:
+            return 'Непвельні дані'
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('keyuser', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
